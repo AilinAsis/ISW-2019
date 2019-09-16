3a935877f3b700540c75736b7cb6cee532d38477
Santiago Prandi <12944591+santiprandi@users.noreply.github.com>
2019-09-16 20:20:48 -0300

    Agregar validaciones de la imágen a subir

--- 08 Implementacion/js/app.js
+++ 08 Implementacion/js/app.js
@@ -1,4 +1,21 @@
-angular.module('app')
-    .controller('appCtrl', function ($scope, $html) {
+angular.module('app', [])
+    .controller('appCtrl', ($scope) => {
 
-    });
+    // Validaciones de la imagen
+    const inputImagen = document.getElementById('inputImagen');
+
+    inputImagen.onclick = () => { this.value = null };
+    inputImagen.onchange = () => {
+        // Validacion del tipo de extension
+        if (inputImagen.files[0].type !== "image/jpeg") {
+            if (inputImagen.files[0].type !== "image/png") {
+                alert("Archivo icorrecto");
+            }
+        }
+
+        // Validacion del tamaño
+        if (inputImagen.files[0].size > 5000000 ) {
+            alert("el archivo es muy grande");
+        }
+    };
+});
