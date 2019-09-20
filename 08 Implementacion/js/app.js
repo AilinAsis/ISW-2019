e445d770b4fc86584a46196565b413eed8d48103
Santiago Prandi <12944591+santiprandi@users.noreply.github.com>
2019-09-20 10:30:49 -0300

    Mejorar validacion de la imagen

--- 08 Implementacion/js/app.js
+++ 08 Implementacion/js/app.js
@@ -1,26 +1,42 @@
 angular.module('app', [])
     .controller('appCtrl', ($scope) => {
-        $scope.caractRest = 180;
+        $scope.caractRest = 180; // Cantidad de caracteres para describir el producto deseado.
 
-        // Validaciones de la imagen
+        /*
+        Indica si la alerta de una imagen incorrecta debe ser mostrada o no.
+        N - No
+        E - Extension incorrecta
+        G - Imagen muy grande.
+        */
+        $scope.mostrarAlertaImagen = 'N';
+
+        // Validaciones de la imagen ==================================================================================
         const inputImagen = document.getElementById('inputImagen');
 
-        inputImagen.onclick = () => { this.value = null };
+        // Evento: clickear boton de imagen.
+        inputImagen.onclick = () => {
+            this.value = null;
+            $scope.mostrarAlertaImagen = 'N';
+            $scope.$apply(); // https://stackoverflow.com/questions/12304728/how-can-i-tell-angularjs-to-refresh
+        };
+        // Evento: elegir una imagen.
         inputImagen.onchange = () => {
             // Validacion del tipo de extension
             if (inputImagen.files[0].type !== 'image/jpeg') {
                 if (inputImagen.files[0].type !== 'image/png') {
-                    alert('Archivo icorrecto');
+                    $scope.mostrarAlertaImagen = 'E';
+                    $scope.$apply();
                 }
             }
 
             // Validacion del tamaño
             if (inputImagen.files[0].size > 5000000) {
-                alert('el archivo es muy grande');
+                $scope.mostrarAlertaImagen = 'G';
+                $scope.$apply();
             }
         };
 
-        // Visualizacion de las formas de pago
+        // Visualizacion de las formas de pago ========================================================================
         $scope.estadoFormaPago = 'N'; // N = Ninguna, E = Efectivo, T = Tarjeta
         $scope.radioEfectivo = false;
         $scope.radioTarjeta = false;
@@ -53,7 +69,7 @@ angular.module('app', [])
             $scope.radioSelecFechaHora = true;
         }
 
-        
 
-       
+
+
     });
